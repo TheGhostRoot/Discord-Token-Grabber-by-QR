08d3e6cf9eaf69c7dbe6c2c0dbb97ebd45e1675a
hirusha-adi <hirushaadi@gmail.com>
2024-06-08 16:39:07 +0530

    get rid of utilities and unwanted setup file

--- setup.py
+++
@@ -1,8 +0,0 @@
-from dtg import utitlites
-
-utitlites.clear_screen()
-utitlites.show_logo()
-utitlites.install_requirements_file()
-utitlites.clear_screen()
-utitlites.show_logo()
-print("+ Setup completed successfully!")
