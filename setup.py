719128b8b20eb1fb864e69b952e53d7e2aeb43a4
hirusha-adi <hirushaadi@gmail.com>
2024-06-08 15:50:36 +0530

    use existing requirements.txt

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@ from dtg import utitlites
 
 utitlites.clear_screen()
 utitlites.show_logo()
-utitlites.create_requirements_file()
 utitlites.install_requirements_file()
-utitlites.remove_requirements_file()
 utitlites.clear_screen()
 utitlites.show_logo()
 print("+ Setup completed successfully!")
-utitlites.pasue_before_destroy_cli()
