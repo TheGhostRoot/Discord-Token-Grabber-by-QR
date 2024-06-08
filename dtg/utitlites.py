77d1d540add01a3018688f9ed20b7378c9975908
hirusha-adi <hirushaadi@gmail.com>
2024-06-08 15:50:22 +0530

    remove unwanted stuff

--- dtg/utitlites.py
+++ dtg/utitlites.py
@@ -1,12 +1,10 @@
 import os
-import platform
 import json
-import time
 from datetime import datetime
 
 
 class OSdiff:
-    if platform.system().lower().startswith('win'):
+    if os.name == 'nt':
         pip = "pip"
         python = "py"
         clear = "cls"
@@ -28,20 +26,10 @@ class Config:
         CLI_SLEEP_TIME = 5
         print("- Please enter a valid value for CLI_SLEEP_TIME in dtg/config.json\n- Defaulting to 5")
 
-    try:
-        DISCORD_WAIT_TIME = int(data["DISCORD_WAIT_TIME"])
-    except:
-        DISCORD_WAIT_TIME = 5
-        print("- Please enter a valid value for CLI_SLEEP_TIME in dtg/config.json\n- Defaulting to 5")
-
     if str(data["SAVE_TO_FILE"]).lower().startswith("y"):
         SAVE_TO_FILE = True
     else:
         SAVE_TO_FILE = False
-
-
-def pasue_before_destroy_cli():
-    time.sleep(Config.CLI_SLEEP_TIME)
 
 
 def save_results_to_file(result: str = None):
@@ -50,19 +38,12 @@ def save_results_to_file(result: str = None):
 
     if "results.txt" in os.listdir(os.getcwd()) == False:
         with open("results.txt", "a", encoding="utf-8") as filem:
-            filem.wirte(f"\n{datetime.now()} - results.txt created!")
+            filem.write(f"\n{datetime.now()} - results.txt created!")
             print("+ Created: results.txt")
 
     with open("results.txt", "a", encoding="utf-8") as filew:
         filew.write(f"\n{result}")
         print("+ Added token to results.txt")
-
-
-def clear_screen():
-    if platform.system().lower().startswith('win'):
-        os.system("cls")
-    else:
-        os.system("clear")
 
 
 def show_logo():
@@ -76,23 +57,6 @@ def show_logo():
          """)
 
 
-def create_requirements_file():
-    with open("requirements.txt", "w") as req_file:
-        req_file.write("""beautifulsoup4
-selenium
-pillow
-lxml""")
-
 
 def install_requirements_file():
     os.system(f"{OSdiff.pip} install -r requirements.txt")
-
-
-def remove_requirements_file():
-    try:
-        os.remove("requirements.txt")
-    except:
-        if OSdiff.windows:
-            os.system("rem /q requirements.txt")
-        if OSdiff.linux:
-            os.systen("rm -rf requirements.txt")
